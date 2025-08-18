c995f06424ed6ef4e73d77c312413aa5c3c5ae8c
Akshay Sundru <akshay.sundrus@gmail.com>
2025-08-19 01:54:38 +0800

    Added login form template and route with basic bootstrap elements

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app.blueprints import main as flaskApp
+from app.forms import LoginForm
 
 @flaskApp.route('/')
 def mainfunc():
@@ -9,7 +10,11 @@ def mainfunc():
 def user(name):
     return "<h1>Hello {}</h1>".format(name)
 
-@flaskApp.route('/indexes')
-def index():
-    user = {'username': 'Akshay'}
-    return render_template('test_page.html', title='Home', user=user)
+@flaskApp.route('/indexes/<username>')
+def index(username):
+    return render_template('test_page.html', title=f'Welcome {username}', username=username)
+
+@flaskApp.route('/login-form')
+def login_form():
+    form = LoginForm()
+    return render_template('login_form.html', title='Sign In', form=form)
