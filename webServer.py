85acc644519a21d54c05f7fdd7f9bd5a72e8eaac
RandomDev92 <billmrayner2@gmail.com>
2025-08-27 00:23:23 +0800

    removed hello world file from akshay branch

--- webServer.py
+++ webServer.py
@@ -2,3 +2,6 @@ from app import create_app
 from app.config import DeploymentConfig
 
 app = create_app(DeploymentConfig)
+
+
+#i recommend running flask through `$flask run` but this is fine if youre actually using it
