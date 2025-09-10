98b3b492ad6d593698d70f57add4b8ab392741a2
andreabong <andreabong1212@gmail.com>
2025-09-10 13:44:04 +0800

    Create route from explore button to dashboard.

--- app/routes.py
+++ app/routes.py
@@ -10,11 +10,15 @@ from sqlalchemy import case, update
 main = Blueprint('main', __name__)
 
 
-
 @main.route('/main_page')
 @main.route('/')
 def home(): 
     return render_template('homepage_purebs.html' )
+
+#guest to explore dashboard
+@main.route('/explore')
+def explore():
+    return render_template('main_page.html')
 
 @main.route('/main-page')
 @login_required
